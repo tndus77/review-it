12e50f7a5801f4a03de363768c22b33e3471c29a
tndus77 <tndus77@sookmyung.ac.kr>
2025-07-11 14:33:30 +0900

    inital setting

--- android/app/src/main/java/com/reviewit/MainActivity.kt
+++ android/app/src/main/java/com/reviewit/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "reviewit"
+  override fun getMainComponentName(): String = "review-it"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
--- ios/reviewit/AppDelegate.swift
+++ ios/reviewit/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "reviewit",
+      withModuleName: "review-it",
       in: window,
       launchOptions: launchOptions
     )
